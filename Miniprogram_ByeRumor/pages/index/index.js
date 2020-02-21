3130ad7aff210519c48d295093cec4df038fd27c
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2020-02-22 01:49:28 +0800

    basic estruture built

--- Miniprogram_ByeRumor/pages/index/index.js
+++ Miniprogram_ByeRumor/pages/index/index.js
@@ -7,6 +7,56 @@ Page({
   tabHandler(e){
     this.setData({
       curIndex: e.currentTarget.dataset.index
-    });
-  }
+    })
+  },
+  enterDetail(){
+    if(this.data.curIndex === 0){
+      this.toHot_rumor();
+    }else if(this.data.curIndex === 1){
+      this.toAntiepic_science();
+    }else{
+      this.toOffic_dynamic();
+    }
+  },
+  toHot_rumor() {
+    wx.navigateTo({
+      url: '../hot_rumor/hot_rumor',
+      events: {
+        getData() {
+          console.log("Data");
+        }
+      },
+      success(res) {
+        res.eventChannel.emit("getData", { data: 0 })
+      }
+    })
+  },
+  toAntiepic_science() {
+    wx.navigateTo({
+      url: '../antiepic_science/antiepic_science',
+      events: {
+        getData() {
+          console.log("Data");
+        }
+      },
+      success(res) {
+        res.eventChannel.emit("getData", { data: 0 })
+      }
+    })
+  },
+  toOffic_dynamic() {
+    wx.navigateTo({
+      url: '../offic_dynamic/offic_dynamic',
+      events: {
+        getData() {
+          console.log("Data");
+        }
+      },
+      success(res) {
+        res.eventChannel.emit("getData", { data: 0 })
+      }
+    })
+  },
+  
+
 })
