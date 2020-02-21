a32732081ad37b509623d989d11d1a3e19f1b2e9
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2020-02-21 23:06:30 +0800

    home page built

--- Miniprogram_ByeRumor/pages/index/index.js
+++ Miniprogram_ByeRumor/pages/index/index.js
@@ -1,5 +1,12 @@
 Page({
-  test (){
-    console.log("123");
+  data: {
+    tabTitles: ["热门谣言", "防疫科普", "官方动态"],
+    curIndex: 0,
+    info: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  },
+  tabHandler(e){
+    this.setData({
+      curIndex: e.currentTarget.dataset.index
+    });
   }
 })
