2fe7830f8014dc4982a1d52536e2f759326dc111
Danjun Xie <40506250+Danjame@users.noreply.github.com>
2020-02-22 12:52:27 +0800

    add swiper

--- Miniprogram_ByeRumor/pages/index/index.js
+++ Miniprogram_ByeRumor/pages/index/index.js
@@ -2,6 +2,7 @@ Page({
   data: {
     tabTitles: ["热门谣言", "防疫科普", "官方动态"],
     curIndex: 0,
+  
     rumors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     science: ["S1", "S2", "S3", "S4", "S5", "S6", "S7", "S8"],
     dynamic: ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8"]
@@ -9,6 +10,11 @@ Page({
   tabHandler(e){
     this.setData({
       curIndex: e.currentTarget.dataset.index
+    })
+  },
+  curChange(e){
+    this.setData({
+      curIndex: e.detail.current
     })
   },
   enterDetail(){
@@ -59,6 +65,60 @@ Page({
       }
     })
   },
-  
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    console.log("123");
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
 
 })
